7d69ab5aa6c38927890ba589f6ac1b49ef5d1d00
Umesh Dangat <umeshdangat@gmail.com>
2020-03-05 11:22:06 -0800

    build go binary for various platform

--- build_grpc_gateway.sh
+++ build_grpc_gateway.sh
@@ -1,3 +1,4 @@
 docker build -t grpc-gateway -f grpc-gateway/Dockerfile .
 docker run grpc-gateway
 docker cp $(docker ps -alq):/code/output/. ./grpc-gateway/
+docker cp $(docker ps -alq):/code/bin/. ./build/install/platypus/bin
