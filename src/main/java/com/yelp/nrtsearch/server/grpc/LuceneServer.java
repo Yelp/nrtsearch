c08344aecfc97a2bebf36b9655b7d2c52756a6ad
Umesh <umesh@yelp.com>
2020-06-22 18:50:56 -0700

    add delay slow down to respond for file copy chunks

--- src/main/java/com/yelp/nrtsearch/server/grpc/LuceneServer.java
+++ src/main/java/com/yelp/nrtsearch/server/grpc/LuceneServer.java
@@ -69,7 +69,6 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.ServerInterceptors;
 import io.grpc.Status;
-import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
 import io.prometheus.client.CollectorRegistry;
 import me.dinowernli.grpc.prometheus.Configuration;
@@ -89,6 +88,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -974,15 +974,6 @@ public class LuceneServer {
         @Override
         public void recvRawFile(FileInfo fileInfoRequest, StreamObserver<RawFileChunk> rawFileChunkStreamObserver) {
             try {
-                // Set up manual flow control for the request stream. It feels backwards to configure the request
-                // stream's flow control using the response stream's observer, but this is the way it is.
-                final ServerCallStreamObserver<RawFileChunk> serverCallStreamObserver =
-                        (ServerCallStreamObserver<RawFileChunk>) rawFileChunkStreamObserver;
-                serverCallStreamObserver.disableAutoInboundFlowControl();
-
-                final OnReadyHandler onReadyHandler = new OnReadyHandler(serverCallStreamObserver);
-                serverCallStreamObserver.setOnReadyHandler(onReadyHandler);
-
                 IndexState indexState = globalState.getIndex(fileInfoRequest.getIndexName());
                 ShardState shardState = indexState.getShard(0);
                 IndexInput luceneFile = shardState.indexDir.openInput(fileInfoRequest.getFileName(), IOContext.DEFAULT);
@@ -993,33 +984,12 @@ public class LuceneServer {
                 long totalRead;
                 totalRead = pos;
                 while (totalRead < len) {
-                    /* we have to wait for the stream to be ready before we can send next chunk on responseObserver
-                    i.e. invoked rawFileChunkStreamObserver.onNext(rawFileChunk);
-                    * */
-                    if (!serverCallStreamObserver.isReady()) {
-                        Thread.sleep(5);
-                        continue;
-                    }
                     int chunkSize = (int) Math.min(buffer.length, (len - totalRead));
                     luceneFile.readBytes(buffer, 0, chunkSize);
                     RawFileChunk rawFileChunk = RawFileChunk.newBuilder().setContent(ByteString.copyFrom(buffer, 0, chunkSize)).build();
                     rawFileChunkStreamObserver.onNext(rawFileChunk);
+                    delay();
                     totalRead += chunkSize;
-                    // Check the provided ServerCallStreamObserver to see if it is still ready to accept more messages.
-                    if (serverCallStreamObserver.isReady()) {
-                        // Signal the sender to send another request. As long as isReady() stays true, the server will keep
-                        // cycling through the loop of onNext() -> request(1)...onNext() -> request(1)... until the client runs
-                        // out of messages and ends the loop (via onCompleted()).
-                        //
-                        // If request() was called here with the argument of more than 1, the server might runs out of receive
-                        // buffer space, and isReady() will turn false. When the receive buffer has sufficiently drained,
-                        // isReady() will turn true, and the serverCallStreamObserver's onReadyHandler will be called to restart
-                        // the message pump.
-                        serverCallStreamObserver.request(1);
-                    } else {
-                        // If not, note that back-pressure has begun.
-                        onReadyHandler.wasReady.set(false);
-                    }
                 }
                 //EOF
                 rawFileChunkStreamObserver.onCompleted();
@@ -1031,6 +1001,17 @@ public class LuceneServer {
                         .augmentDescription(e.getMessage())
                         .asRuntimeException());
             }
+        }
+
+        /** Add a delay of 1ms (inclusive) to 10ms(inclusive) between sending file chunks
+         * In absence of this we can
+         * @throws InterruptedException
+         */
+        /**/
+        private void delay() throws InterruptedException {
+            Random random = new Random();
+            int val = random.nextInt(10);
+            Thread.sleep(val+1);
         }
 
         @Override
@@ -1050,6 +1031,7 @@ public class LuceneServer {
                         .asRuntimeException());
             }
         }
+
 
         @Override
         public void copyFiles(CopyFiles request, StreamObserver<TransferStatus> responseObserver) {
