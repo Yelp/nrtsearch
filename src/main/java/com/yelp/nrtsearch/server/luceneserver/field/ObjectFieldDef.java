d1326963b4b43d1c58b05818203b94baa54e3a17
Tao Yu <taoyu@yelp.com>
2020-11-19 13:58:22 -0800

    update generated file and fix format

--- src/main/java/com/yelp/nrtsearch/server/luceneserver/field/ObjectFieldDef.java
+++ src/main/java/com/yelp/nrtsearch/server/luceneserver/field/ObjectFieldDef.java
@@ -1,76 +1,93 @@
+/*
+ * Copyright 2020 Yelp Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yelp.nrtsearch.server.luceneserver.field;
 
 import com.google.gson.Gson;
 import com.yelp.nrtsearch.server.grpc.Field;
 import com.yelp.nrtsearch.server.luceneserver.IndexState;
-import org.apache.lucene.document.Document;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import org.apache.lucene.document.Document;
 
-public class ObjectFieldDef extends IndexableFieldDef{
+public class ObjectFieldDef extends IndexableFieldDef {
 
-    private final Gson gson;
+  private final Gson gson;
 
-    protected ObjectFieldDef(String name, Field requestField) {
-        super(name, requestField);
-        gson = new Gson();
-    }
+  protected ObjectFieldDef(String name, Field requestField) {
+    super(name, requestField);
+    gson = new Gson();
+  }
 
-    @Override
-    public String getType() {
-        return "object";
-    }
+  @Override
+  public String getType() {
+    return "object";
+  }
 
-    @Override
-    public void parseDocumentField(Document document, List<String> fieldValues, List<List<String>> facetHierarchyPaths) {
-    }
+  @Override
+  public void parseDocumentField(
+      Document document, List<String> fieldValues, List<List<String>> facetHierarchyPaths) {}
 
-    @Override
-    public void parseFieldWithChildren(
-            Document document, List<String> fieldValues, List<List<String>> facetHierarchyPaths) {
-        List<Map<String, Object>> fieldValueMaps = fieldValues
-                .stream().map(e -> gson.fromJson(e, Map.class)).collect(Collectors.toList());
-        parseFieldWithChildrenObject(document, fieldValueMaps, facetHierarchyPaths);
-    }
+  @Override
+  @SuppressWarnings("unchecked")
+  public void parseFieldWithChildren(
+      Document document, List<String> fieldValues, List<List<String>> facetHierarchyPaths) {
+    List<Map<String, Object>> fieldValueMaps = new ArrayList<>();
+    fieldValues.stream().map(e -> gson.fromJson(e, Map.class)).forEach(e -> fieldValueMaps.add(e));
+    parseFieldWithChildrenObject(document, fieldValueMaps, facetHierarchyPaths);
+  }
 
-    public void parseFieldWithChildrenObject(
-            Document document, List<Map<String, Object>> fieldValues, List<List<String>> facetHierarchyPaths
-    ) {
-        for (Map.Entry<String, IndexableFieldDef> childField: childFields.entrySet()) {
-            String[] keys = childField.getKey().split(IndexState.CHILD_FIELD_SEPARATOR);
-            String key = keys[keys.length - 1];
-            if(childField.getValue().getType().equals("object")) {
-                List<Map<String, Object>> childrenValues = new ArrayList<>();
-                for (Map<String, Object> fieldValue: fieldValues) {
-                    Object childValue = fieldValue.get(key);
-                    if (childValue != null) {
-                        if (childValue instanceof Map) {
-                            childrenValues.add((Map<String, Object>) childValue);
-                        } else if (childValue instanceof List) {
-                            childrenValues.addAll((List<Map<String, Object>>) childValue);
-                        } else {
-                            throw new IllegalArgumentException("Invalid data");
-                        }
-                    }
-                }
-                ((ObjectFieldDef) childField.getValue()).parseFieldWithChildrenObject(document, childrenValues, facetHierarchyPaths);
+  public void parseFieldWithChildrenObject(
+      Document document,
+      List<Map<String, Object>> fieldValues,
+      List<List<String>> facetHierarchyPaths) {
+    for (Map.Entry<String, IndexableFieldDef> childField : childFields.entrySet()) {
+      String[] keys = childField.getKey().split(IndexState.CHILD_FIELD_SEPARATOR);
+      String key = keys[keys.length - 1];
+      if (childField.getValue().getType().equals("object")) {
+        List<Map<String, Object>> childrenValues = new ArrayList<>();
+        for (Map<String, Object> fieldValue : fieldValues) {
+          Object childValue = fieldValue.get(key);
+          if (childValue != null) {
+            if (childValue instanceof Map) {
+              childrenValues.add((Map<String, Object>) childValue);
+            } else if (childValue instanceof List) {
+              childrenValues.addAll((List<Map<String, Object>>) childValue);
             } else {
-                List<String> childrenValues = new ArrayList<>();
-                for (Map<String, Object> fieldValue: fieldValues) {
-                    Object childValue = fieldValue.get(key);
-                    if (childValue != null) {
-                        if (childValue instanceof List) {
-                            ((List<Object>) childValue).stream().forEach(e -> childrenValues.add(String.valueOf(e)));
-                        } else {
-                            childrenValues.add(String.valueOf(childValue));
-                        }
-                    }
-                }
-                childField.getValue().parseFieldWithChildren(document, childrenValues, facetHierarchyPaths);
+              throw new IllegalArgumentException("Invalid data");
             }
+          }
         }
+        ((ObjectFieldDef) childField.getValue())
+            .parseFieldWithChildrenObject(document, childrenValues, facetHierarchyPaths);
+      } else {
+        List<String> childrenValues = new ArrayList<>();
+        for (Map<String, Object> fieldValue : fieldValues) {
+          Object childValue = fieldValue.get(key);
+          if (childValue != null) {
+            if (childValue instanceof List) {
+              ((List<Object>) childValue)
+                  .stream().forEach(e -> childrenValues.add(String.valueOf(e)));
+            } else {
+              childrenValues.add(String.valueOf(childValue));
+            }
+          }
+        }
+        childField.getValue().parseFieldWithChildren(document, childrenValues, facetHierarchyPaths);
+      }
     }
+  }
 }
--- src/test/java/com/yelp/nrtsearch/server/luceneserver/field/ObjectFieldDefTest.java
+++ src/test/java/com/yelp/nrtsearch/server/luceneserver/field/ObjectFieldDefTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Yelp Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yelp.nrtsearch.server.luceneserver.field;
 
 import static org.junit.Assert.assertEquals;
@@ -6,77 +21,74 @@ import com.google.gson.Gson;
 import com.yelp.nrtsearch.server.grpc.*;
 import com.yelp.nrtsearch.server.luceneserver.ServerTestCase;
 import io.grpc.testing.GrpcCleanupRule;
+import java.io.IOException;
+import java.util.*;
 import org.junit.ClassRule;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.util.*;
-
 public class ObjectFieldDefTest extends ServerTestCase {
 
-    @ClassRule
-    public static final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+  @ClassRule public static final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
-    protected List<String> getIndices() {
-        return Collections.singletonList(DEFAULT_TEST_INDEX);
+  protected List<String> getIndices() {
+    return Collections.singletonList(DEFAULT_TEST_INDEX);
+  }
+
+  protected FieldDefRequest getIndexDef(String name) throws IOException {
+    return getFieldsFromResourceFile("/field/registerFieldsObject.json");
+  }
+
+  protected void initIndex(String name) throws Exception {
+    List<AddDocumentRequest> docs = new ArrayList<>();
+    Gson gson = new Gson();
+    Map<String, Object> map = new HashMap<>();
+    map.put("hours", List.of(1, 2));
+    map.put("zipcode", List.of("94105", "EC2Y8NE"));
+    AddDocumentRequest request =
+        AddDocumentRequest.newBuilder()
+            .setIndexName(name)
+            .putFields(
+                "doc_id", AddDocumentRequest.MultiValuedField.newBuilder().addValue("1").build())
+            .putFields(
+                "delivery_areas",
+                AddDocumentRequest.MultiValuedField.newBuilder().addValue(gson.toJson(map)).build())
+            .build();
+    docs.add(request);
+    addDocuments(docs.stream());
+  }
+
+  @Test
+  public void testObjectChildFields() {
+    SearchResponse response =
+        doQuery(
+            Query.newBuilder()
+                .setMatchQuery(
+                    MatchQuery.newBuilder().setField("delivery_areas.hours").setQuery("1").build())
+                .build(),
+            List.of("doc_id"));
+    assertFields(response, "1");
+  }
+
+  private SearchResponse doQuery(Query query, List<String> fields) {
+    return getGrpcServer()
+        .getBlockingStub()
+        .search(
+            SearchRequest.newBuilder()
+                .setIndexName(DEFAULT_TEST_INDEX)
+                .setStartHit(0)
+                .setTopHits(10)
+                .addAllRetrieveFields(fields)
+                .setQuery(query)
+                .build());
+  }
+
+  private void assertFields(SearchResponse response, String... expectedIds) {
+    Set<String> seenSet = new HashSet<>();
+    for (SearchResponse.Hit hit : response.getHitsList()) {
+      String id = hit.getFieldsOrThrow("doc_id").getFieldValue(0).getTextValue();
+      seenSet.add(id);
     }
-
-    protected FieldDefRequest getIndexDef(String name) throws IOException {
-        return getFieldsFromResourceFile("/field/registerFieldsObject.json");
-    }
-
-    protected void initIndex(String name) throws Exception {
-        List<AddDocumentRequest> docs = new ArrayList<>();
-        Gson gson = new Gson();
-        Map<String, Object> map = new HashMap<>();
-        map.put("hours", List.of(1, 2));
-        map.put("zipcode", List.of("94105", "EC2Y8NE"));
-        AddDocumentRequest request =
-                AddDocumentRequest.newBuilder()
-                        .setIndexName(name)
-                        .putFields("doc_id", AddDocumentRequest.MultiValuedField.newBuilder().addValue("1").build())
-                        .putFields("delivery_areas", AddDocumentRequest.MultiValuedField.newBuilder().addValue(gson.toJson(map)).build())
-                        .build();
-        docs.add(request);
-        addDocuments(docs.stream());
-    }
-
-    @Test
-    public void testObjectChildFields() {
-        SearchResponse response =
-                doQuery(
-                        Query.newBuilder()
-                        .setMatchQuery(
-                                MatchQuery.newBuilder().setField("delivery_areas.hours").setQuery("1").build()
-                        )
-                        .build(), List.of("doc_id")
-                );
-        assertFields(response, "1");
-    }
-
-    private SearchResponse doQuery(Query query, List<String> fields) {
-        return getGrpcServer()
-                .getBlockingStub()
-                .search(
-                        SearchRequest.newBuilder().setIndexName(DEFAULT_TEST_INDEX)
-                        .setStartHit(0)
-                        .setTopHits(10)
-                        .addAllRetrieveFields(fields)
-                        .setQuery(query)
-                        .build()
-                );
-    }
-
-    private void assertFields(SearchResponse response, String... expectedIds) {
-        Set<String> seenSet = new HashSet<>();
-        for(SearchResponse.Hit hit: response.getHitsList()) {
-            String id = hit.getFieldsOrThrow("doc_id").getFieldValue(0).getTextValue();
-            seenSet.add(id);
-
-        }
-        Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedIds));
-        assertEquals(seenSet, expectedSet);
-    }
-
-
+    Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedIds));
+    assertEquals(seenSet, expectedSet);
+  }
 }
