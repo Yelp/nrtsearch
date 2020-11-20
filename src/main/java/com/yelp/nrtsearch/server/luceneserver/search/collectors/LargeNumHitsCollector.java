4fbf59ea6444b23987a8d4ddf45f032cd781e371
Andrew Prudhomme <asp@yelp.com>
2020-11-20 13:12:54 -0800

    Build search request into SearchContext (#187)

---
+++ src/main/java/com/yelp/nrtsearch/server/luceneserver/search/collectors/LargeNumHitsCollector.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Yelp Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yelp.nrtsearch.server.luceneserver.search.collectors;
+
+import com.yelp.nrtsearch.server.grpc.SearchRequest;
+import com.yelp.nrtsearch.server.grpc.SearchResponse;
+import com.yelp.nrtsearch.server.luceneserver.LargeNumHitsTopDocsCollectorManagerCreator;
+import org.apache.lucene.search.Collector;
+import org.apache.lucene.search.CollectorManager;
+import org.apache.lucene.search.LargeNumHitsTopDocsCollector;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+
+/** Collector for getting a large number of documents ranked by relevance score. */
+public class LargeNumHitsCollector implements DocCollector {
+
+  private final CollectorManager<LargeNumHitsTopDocsCollector, TopDocs> manager;
+
+  public LargeNumHitsCollector(SearchRequest searchRequest) {
+    int topHits = getNumHitsToCollect(searchRequest);
+    manager = LargeNumHitsTopDocsCollectorManagerCreator.createSharedManager(topHits);
+  }
+
+  @Override
+  public CollectorManager<? extends Collector, ? extends TopDocs> getManager() {
+    return manager;
+  }
+
+  @Override
+  public void fillHitRanking(SearchResponse.Hit.Builder hitResponse, ScoreDoc scoreDoc) {
+    if (!Float.isNaN(scoreDoc.score)) {
+      hitResponse.setScore(scoreDoc.score);
+    }
+  }
+
+  @Override
+  public void fillLastHit(SearchResponse.SearchState.Builder stateBuilder, ScoreDoc lastHit) {
+    stateBuilder.setLastScore(lastHit.score);
+  }
+}
