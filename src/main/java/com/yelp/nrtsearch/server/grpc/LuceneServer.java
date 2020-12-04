77f4585ecc4721b03955a0e49b859948ba955614
Sarthak Nandi <sarthakn@yelp.com>
2020-12-03 17:52:09 -0800

    Cleaning up snapshot and temporary files from interrupted backups when index is started

--- src/main/java/com/yelp/nrtsearch/server/grpc/LuceneServer.java
+++ src/main/java/com/yelp/nrtsearch/server/grpc/LuceneServer.java
@@ -250,6 +250,7 @@ public class LuceneServer {
     private final Archiver archiver;
     private final CollectorRegistry collectorRegistry;
     private final ThreadPoolExecutor searchThreadPoolExecutor;
+    private final String archiveDirectory;
 
     LuceneServerImpl(
         GlobalState globalState,
@@ -259,6 +260,7 @@ public class LuceneServer {
         List<Plugin> plugins) {
       this.globalState = globalState;
       this.archiver = archiver;
+      this.archiveDirectory = configuration.getArchiveDirectory();
       this.collectorRegistry = collectorRegistry;
       this.searchThreadPoolExecutor =
           ThreadPoolExecutorFactory.getThreadPoolExecutor(
@@ -474,7 +476,7 @@ public class LuceneServer {
         StartIndexRequest startIndexRequest, StreamObserver<StartIndexResponse> responseObserver) {
       try {
         IndexState indexState = null;
-        StartIndexHandler startIndexHandler = new StartIndexHandler(archiver);
+        StartIndexHandler startIndexHandler = new StartIndexHandler(archiver, archiveDirectory);
         indexState =
             globalState.getIndex(startIndexRequest.getIndexName(), startIndexRequest.hasRestore());
         StartIndexResponse reply = startIndexHandler.handle(indexState, startIndexRequest);
@@ -1177,7 +1179,7 @@ public class LuceneServer {
       try {
         IndexState indexState = globalState.getIndex(backupIndexRequest.getIndexName());
         BackupIndexRequestHandler backupIndexRequestHandler =
-            new BackupIndexRequestHandler(archiver);
+            new BackupIndexRequestHandler(archiver, archiveDirectory);
         BackupIndexResponse reply =
             backupIndexRequestHandler.handle(indexState, backupIndexRequest);
         logger.info(String.format("BackupRequestHandler returned results %s", reply.toString()));
