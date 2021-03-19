d2932df0b7fe127a27ce3d1565bacc81012ce87c
Sarthak Nandi <sarthakn@yelp.com>
2021-03-18 18:42:41 -0700

    Failing grpc-gateway build if any command errors

--- build_grpc_gateway.sh
+++ build_grpc_gateway.sh
@@ -1,10 +1,5 @@
+set -euxo pipefail
 docker build -t grpc-gateway -f grpc-gateway/Dockerfile .
-status=$?
-if [ $status -ne 0 ];
-then
-  echo "Unable to build grpc-gateway";
-  exit $status;
-fi
 docker run grpc-gateway
 docker cp $(docker ps -alq):/code/output/yelp/nrtsearch/. ./grpc-gateway/
 docker cp $(docker ps -alq):/code/bin/. ./build/install/nrtsearch/bin
