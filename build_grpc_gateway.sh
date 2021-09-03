ed7d3fe0eefef1054f230a348932a3db6e9f9ee8
Andrew Prudhomme <asp@yelp.com>
2021-09-03 15:51:52 -0700

    Fix race condition in gateway docker build script (#372)

--- build_grpc_gateway.sh
+++ build_grpc_gateway.sh
@@ -1,6 +1,7 @@
 set -euxo pipefail
 docker build -t grpc-gateway -f grpc-gateway/Dockerfile .
-docker run grpc-gateway
-docker cp $(docker ps -alq):/code/output/yelp/nrtsearch/. ./grpc-gateway/
-docker cp $(docker ps -alq):/code/output/github.com/Yelp/nrtsearch/. ./grpc-gateway/
-docker cp $(docker ps -alq):/code/bin/. ./build/install/nrtsearch/bin
+CID=$(docker run -d grpc-gateway)
+docker cp $CID:/code/output/yelp/nrtsearch/. ./grpc-gateway/
+docker cp $CID:/code/output/github.com/Yelp/nrtsearch/. ./grpc-gateway/
+docker cp $CID:/code/bin/. ./build/install/nrtsearch/bin
+docker container rm $CID
