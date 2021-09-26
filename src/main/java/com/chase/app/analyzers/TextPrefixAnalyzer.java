456ce59016b36232aebf41e30fb7a25c88c111d6
Nimrod Dolev <nimrodavid@gmail.com>
2021-09-26 17:02:43 +0300

    cleanup

--- src/main/java/com/chase/app/analyzers/TextPrefixAnalyzer.java
+++ src/main/java/com/chase/app/analyzers/TextPrefixAnalyzer.java
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
- package com.chase.app.analyzers;
+package com.chase.app.analyzers;
 
 import com.chase.app.tokenFilters.EdgeNgramFilterHelper;
 import com.chase.app.tokenFilters.ShingleFilterHelper;
