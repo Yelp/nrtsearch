6c2a73978d447b9b2000df294de3dbcfdcbd9a76
Sarthak Nandi <sarthakn@yelp.com>
2022-04-05 20:35:35 -0700

    Spotless apply

--- src/main/java/com/yelp/nrtsearch/server/plugins/CustomRequestPlugin.java
+++ src/main/java/com/yelp/nrtsearch/server/plugins/CustomRequestPlugin.java
@@ -1,23 +1,38 @@
+/*
+ * Copyright 2022 Yelp Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yelp.nrtsearch.server.plugins;
 
 import java.util.List;
 import java.util.Map;
 
 /**
- * Plugin interface that allows adding one or more routes and their corresponding processors
- * which can be called via the `custom` RPC. This can be implemented by plugins to modify behavior
- * without requiring updating config files and restarting nrtsearch.
+ * Plugin interface that allows adding one or more routes and their corresponding processors which
+ * can be called via the `custom` RPC. This can be implemented by plugins to modify behavior without
+ * requiring updating config files and restarting nrtsearch.
  */
 public interface CustomRequestPlugin {
 
-    interface Route {
+  interface Route {
 
-        String path();
+    String path();
 
-        Map<String, String> process(Map<String, String> request);
-    }
+    Map<String, String> process(Map<String, String> request);
+  }
 
-    String id();
+  String id();
 
-    List<Route> getRoutes();
+  List<Route> getRoutes();
 }
