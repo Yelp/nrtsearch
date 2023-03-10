cc61176ad39cdac4b1f887f8c7d7af7915bca71c
Tao Yu <taoyu@yelp.com>
2023-03-10 09:46:26 -0800

    fix tests

--- src/test/java/com/yelp/nrtsearch/server/grpc/QueryTest.java
+++ src/test/java/com/yelp/nrtsearch/server/grpc/QueryTest.java
@@ -862,20 +862,20 @@ public class QueryTest {
     responseTester.accept(searchResponseExplained);
     String expectedExplain =
         "0.40773362 = weight(vendor_name:\"second second\"~1 in 1) [], result of:\n"
-            + "      0.40773362 = score(freq=0.5), computed as boost * idf * tf from:\n"
-            + "        1.3862944 = idf, sum of:\n"
-            + "          0.6931472 = idf, computed as log(1 + (N - n + 0.5) / (n + 0.5)) from:\n"
-            + "            1 = n, number of documents containing term\n"
-            + "            2 = N, total number of documents with field\n"
-            + "          0.6931472 = idf, computed as log(1 + (N - n + 0.5) / (n + 0.5)) from:\n"
-            + "            1 = n, number of documents containing term\n"
-            + "            2 = N, total number of documents with field\n"
-            + "        0.29411763 = tf, computed as freq / (freq + k1 * (1 - b + b * dl / avgdl)) from:\n"
-            + "          0.5 = phraseFreq=0.5\n"
-            + "          1.2 = k1, term saturation parameter\n"
-            + "          0.75 = b, length normalization parameter\n"
-            + "          4.0 = dl, length of field\n"
-            + "          4.0 = avgdl, average length of field";
+            + "  0.40773362 = score(freq=0.5), computed as boost * idf * tf from:\n"
+            + "    1.3862944 = idf, sum of:\n"
+            + "      0.6931472 = idf, computed as log(1 + (N - n + 0.5) / (n + 0.5)) from:\n"
+            + "        1 = n, number of documents containing term\n"
+            + "        2 = N, total number of documents with field\n"
+            + "      0.6931472 = idf, computed as log(1 + (N - n + 0.5) / (n + 0.5)) from:\n"
+            + "        1 = n, number of documents containing term\n"
+            + "        2 = N, total number of documents with field\n"
+            + "    0.29411763 = tf, computed as freq / (freq + k1 * (1 - b + b * dl / avgdl)) from:\n"
+            + "      0.5 = phraseFreq=0.5\n"
+            + "      1.2 = k1, term saturation parameter\n"
+            + "      0.75 = b, length normalization parameter\n"
+            + "      4.0 = dl, length of field\n"
+            + "      4.0 = avgdl, average length of field";
     for (int i = 0; i < searchResponse.getHitsCount(); i++) {
       SearchResponse.Hit hit = searchResponse.getHits(i);
       SearchResponse.Hit explainedHit = searchResponseExplained.getHits(i);
@@ -883,7 +883,7 @@ public class QueryTest {
       SearchResponse.Hit explainedHitWithoutExplain =
           explainedHit.toBuilder().setExplain("").build();
       assertEquals(hitWithoutExplain, explainedHitWithoutExplain);
-      assertEquals(explainedHit.getExplain(), expectedExplain);
+      assertEquals(expectedExplain, explainedHit.getExplain().trim());
     }
   }
 
