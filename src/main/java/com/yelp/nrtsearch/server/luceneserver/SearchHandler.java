af684f71b5d4e657ff089e81c2295983d7aabda8
Xinyi Wang <xinyi@yelp.com>
2024-06-27 17:30:06 -0700

    check the total hits threshold not zero before setting it

--- src/main/java/com/yelp/nrtsearch/server/luceneserver/SearchHandler.java
+++ src/main/java/com/yelp/nrtsearch/server/luceneserver/SearchHandler.java
@@ -183,12 +183,14 @@ public class SearchHandler implements Handler<SearchRequest, SearchResponse> {
         searcherResult =
             s.searcher.search(
                 searchContext.getQuery(), searchContext.getCollector().getWrappedManager());
-        searcherResult.getTopDocs().totalHits =
-            new org.apache.lucene.search.TotalHits(
-                Math.min(
-                    searchRequest.getTotalHitsThreshold(),
-                    searcherResult.getTopDocs().totalHits.value),
-                searcherResult.getTopDocs().totalHits.relation);
+        if (searchRequest.getTotalHitsThreshold() != 0) {
+          searcherResult.getTopDocs().totalHits =
+              new org.apache.lucene.search.TotalHits(
+                  Math.min(
+                      searchRequest.getTotalHitsThreshold(),
+                      searcherResult.getTopDocs().totalHits.value),
+                  searcherResult.getTopDocs().totalHits.relation);
+        }
         hits = searcherResult.getTopDocs();
       }
 
