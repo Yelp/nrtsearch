0af468e5595210ad0fbea55b77fa6700ab900b4e
Andrew Prudhomme <asp@yelp.com>
2025-10-17 14:51:49 -0700

    Refactor CopyOneFile to match public api of Lucene version

--- src/main/java/org/apache/lucene/replicator/nrt/CopyOneFile.java
+++ src/main/java/org/apache/lucene/replicator/nrt/CopyOneFile.java
@@ -17,18 +17,59 @@ package org.apache.lucene.replicator.nrt;
 
 import java.io.Closeable;
 import java.io.IOException;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexOutput;
 
 /**
- * Interface to replace the Lucene CopyOneFile class, so that we can use separate implementations
- * for gRPC vs remote (S3) replication.
+ * Abstract class to replace the Lucene CopyOneFile class, so that we can use separate
+ * implementations for gRPC vs remote (S3) replication.
  */
-public interface CopyOneFile extends Closeable {
+public abstract class CopyOneFile implements Closeable {
+  public final String name;
+  public final String tmpName;
+  public final FileMetaData metaData;
+  public final long bytesToCopy;
+  protected final IndexOutput out;
+  protected final ReplicaNode dest;
+
+  /**
+   * Constructor
+   *
+   * @param dest destination ReplicaNode
+   * @param name file name
+   * @param metaData file metadata
+   * @throws IOException on error
+   */
+  protected CopyOneFile(ReplicaNode dest, String name, FileMetaData metaData) throws IOException {
+    this.name = name;
+    this.dest = dest;
+    // TODO: pass correct IOCtx, e.g. seg total size
+    out = dest.createTempOutput(name, "copy", IOContext.DEFAULT);
+    tmpName = out.getName();
+
+    // last 8 bytes are checksum, which we write ourselves after copying all bytes and confirming
+    // checksum:
+    bytesToCopy = metaData.length() - Long.BYTES;
+
+    if (Node.VERBOSE_FILES) {
+      dest.message(
+          "file "
+              + name
+              + ": start copying to tmp file "
+              + tmpName
+              + " length="
+              + (8 + bytesToCopy));
+    }
+
+    this.metaData = metaData;
+  }
+
   /**
    * Get the number of bytes copied so far.
    *
    * @return number of bytes copied so far
    */
-  long getBytesCopied();
+  public abstract long getBytesCopied();
 
   /**
    * Perform one unit of work copying the file.
@@ -36,33 +77,11 @@ public interface CopyOneFile extends Closeable {
    * @return true if the file is fully copied, false otherwise
    * @throws IOException on error
    */
-  boolean visit() throws IOException;
+  public abstract boolean visit() throws IOException;
 
-  /**
-   * Get the file metadata.
-   *
-   * @return file metadata
-   */
-  FileMetaData getFileMetaData();
-
-  /**
-   * Get the file name.
-   *
-   * @return file name
-   */
-  String getFileName();
-
-  /**
-   * Get the temporary file name used during copying.
-   *
-   * @return temporary file name
-   */
-  String getFileTmpName();
-
-  /**
-   * Get the total number of bytes to copy.
-   *
-   * @return total number of bytes to copy
-   */
-  long getBytesToCopy();
+  @Override
+  public void close() throws IOException {
+    out.close();
+    dest.finishCopyFile(name);
+  }
 }
